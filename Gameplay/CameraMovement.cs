d981a434e423df6d68c5283b7d3ea8178b364e62
Malte Schönberger <32910193+mschonberger@users.noreply.github.com>
2022-04-14 06:32:18 +0200

    Updated CameraMovements with comments

--- Gameplay/CameraMovement.cs
+++ Gameplay/CameraMovement.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*Die Klasse CameraMovement wird dazu verwendet, um die Kamera dem Spieler möglichst smooth folgen zu lassen. 
+Sie muss an ein Kamera GameObject gebunden werden. Desto kleiner der Smoothing Wert (zum Beispiel 0.1), um so langsamer zieht die Kamera nach.*/
+
 public class CameraMovement : MonoBehaviour
 {
     public Transform target;
@@ -15,6 +18,14 @@ public class CameraMovement : MonoBehaviour
         
     }
 
+/*LateUpdate wird ganz zum Ende eines Frames ausgeführt. 
+Der Spieler soll sich innerhalb des regulären Updates bewegen und die Kamera soll im LateUpdate hinterherziehen. 
+Wenn beide zusammen updaten, könnte dies zu Kamera ruckeln führen.
+
+lerp nutzt lineare Interpolation, um die Distanz zwischen dem Target und der Kamera langsam in wenig % anzugleichen.
+Der Z-Vektor von targetPosition wird festgesetzt, damit die Kamera nicht in der Spielebene verschwindet.
+*/
+
     void LateUpdate()
     {
         if(transform.position != target.position)
@@ -24,10 +35,7 @@ public class CameraMovement : MonoBehaviour
             targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
             targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
             
-            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing); //lerp nutzt lineare Interpolation, um die Distanz zwischen dem Target und der Kamera langsam in wenig % anzugleichen
-        
-            
-
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
         }
     }
 }
