08c4ba7a97c63b9dd7097c98fe1dc7dd56e6e449
Malte Schönberger <32910193+mschonberger@users.noreply.github.com>
2022-04-14 09:34:38 +0200

    Added more Growthrates and Fluctuating for XP Gain

--- Monster/MonsterBase.cs
+++ Monster/MonsterBase.cs
@@ -42,8 +42,35 @@ public class MonsterBase : ScriptableObject
         } else if (growthRate == GrowthRate.MediumFast) 
         {
             return level * level * level;
+        } else if (growthRate == GrowthRate.MediumSlow)
+        {
+            return 6 * (level * level * level) / 5 - 15 * (level * level) + 100 * level - 140;
+        }
+        else if (growthRate == GrowthRate.Slow)
+        {
+            return 5 * (level * level * level) / 4;
+        }
+        else if (growthRate == GrowthRate.Fluctuating)
+        {
+            return GetFluctuating(level);
         }
         return -1;
+    }
+    
+    public int GetFluctuating(int level)
+    {
+        if (level < 15)
+        {
+            return Mathf.FloorToInt(Mathf.Pow(level, 3) * ((Mathf.Floor((level + 1) / 3) + 24) / 50));
+        }
+        else if (level >= 15 && level < 36)
+        {
+            return Mathf.FloorToInt(Mathf.Pow(level, 3) * ((level + 14) / 50));
+        }
+        else
+        {
+            return Mathf.FloorToInt(Mathf.Pow(level, 3) * ((Mathf.Floor(level / 2) + 32) / 50));
+        }
     }
 
     public string Name {
@@ -161,7 +188,9 @@ public enum GrowthRate
 {
     Fast,
     MediumFast,
-    
+    MediumSlow,
+    Slow
+    Fluctuating
 }
 
 public enum Stat
