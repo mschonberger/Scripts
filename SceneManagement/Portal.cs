1b2bc938e89ef657cf2a36c28246d5e7989608e2
Malte Schönberger <malte.schoenberger@gmail.com>
2022-05-18 17:37:12 +0200

    Added Fader
    - Fader is for better looking Scene Transition

--- SceneManagement/Portal.cs
+++ SceneManagement/Portal.cs
@@ -11,6 +11,7 @@ public class Portal : MonoBehaviour, INTERFACEPlayerTriggerable
     [SerializeField] DestinationIdentifier destinationPortal;
 
     PlayerController player;
+    Fader fader;
 
     public void OnPlayerTriggered(PlayerController player)
     {
@@ -18,17 +19,24 @@ public class Portal : MonoBehaviour, INTERFACEPlayerTriggerable
         StartCoroutine(SwitchScene());
     }
 
+    private void Start()
+    {
+        fader = FindObjectOfType<Fader>();
+    }
+
     IEnumerator SwitchScene()
     {
         DontDestroyOnLoad(gameObject);
 
         GameController.Instance.PauseGame(true);
+        yield return fader.FadeIn(0.5f);
 
         yield return SceneManager.LoadSceneAsync(sceneToLoad);
 
         var destinationPortal = FindObjectsOfType<Portal>().First(x => x != this && x.destinationPortal == this.destinationPortal);
         player.Character.SetPositionAndSnapToTile(destinationPortal.SpawnPoint.position);
 
+        yield return fader.FadeOut(0.5f);
         GameController.Instance.PauseGame(false);
 
         Destroy(gameObject);
